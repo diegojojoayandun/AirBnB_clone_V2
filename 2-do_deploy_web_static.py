784c06efb001c7222374c192449b3617ef923487
diegojojoayandun <3922@holbertonschool.com>
2022-05-21 17:41:39 -0500

    Add task2 deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
+
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['52.90.59.238', '54.197.203.132']
+
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+
+        return True
+    except:
+        return False
