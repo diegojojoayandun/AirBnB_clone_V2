1e8ae4327eb22b4b19051ba52383b984f43b5477
diegojojoayandun <3922@holbertonschool.com>
2022-05-22 10:08:44 -0500

    Add puppet manifest task advanced 101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -17,8 +17,7 @@ exec {'d':
   provider => shell,
 }
 exec {'e':
-  command  => '/usr/bin/env echo "<html><head></head><body>Holberton School</body></html>" >
-  /data/web_static/releases/test/index.html',
+  command  => '/usr/bin/env echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html',
   provider => shell,
 }
 exec {'f':
@@ -30,8 +29,7 @@ exec {'g':
   provider => shell,
 }
 exec {'h':
-  command  => '/usr/bin/env sed -i "/listen 80 default_server/a location /hbnb_static/
-  { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+  command  => '/usr/bin/env sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
   provider => shell,
 }
 exec {'i':
