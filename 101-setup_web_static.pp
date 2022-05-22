bbb753115f8a25c9de07c5c39761110ac7dfa89c
diegojojoayandun <3922@holbertonschool.com>
2022-05-22 11:41:33 -0500

    Add puppet manifest task advanced 101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,37 +1,38 @@
 # Puppet manifest - preprares web server for deploy, update, install nginx
+
 exec { 'apt-get-update':
-  command  => 'apt-get -y update',
+  command  => 'sudo apt-get -y update',
   provider => 'shell',
 }
 exec {'Install Nginx':
-  command  => 'apt-get -y install nginx',
+  command  => 'sudo apt-get -y install nginx',
   provider => 'shell',
 }
 exec {'Create test directory':
-  command  => 'mkdir -p /data/web_static/releases/test/',
+  command  => 'sudo mkdir -p /data/web_static/releases/test/',
   provider => 'shell',
 }
 exec {'Create shared directory':
-  command  => 'mkdir -p /data/web_static/shared/',
+  command  => 'sudo mkdir -p /data/web_static/shared/',
   provider => 'shell',
 }
 exec {'create index.html':
-  command  => 'echo "Holberton School" > /data/web_static/releases/test/index.html',
-  provider => 'shell',
-}
-exec {'changed ownership':
-  command  => 'chown -R ubuntu:ubuntu /data',
+  command  => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
   provider => 'shell',
 }
 exec {'create symbolic link':
   command  => 'ln -sf /data/web_static/releases/test /data/web_static/current',
   provider => 'shell',
 }
+exec {'changed ownership':
+  command  => 'sudo chown -R ubuntu:ubuntu /data',
+  provider => 'shell',
+}
 exec {'change default':
-  command  => 'sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
   provider => 'shell',
 }
 exec {'restart nginx sevice':
-  command  => 'service nginx restart',
+  command  => 'sudo service nginx restart',
   provider => 'shell',
 }
