ba40a45570ea117d40342b7fd692cd44e3e1a55e
diegojojoayandun <3922@holbertonschool.com>
2022-05-22 11:32:02 -0500

    Add puppet manifest task advanced 101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,38 +1,37 @@
 # Puppet manifest - preprares web server for deploy, update, install nginx
-# create some needed directorys, files and symbolic link
 exec { 'apt-get-update':
-  command => 'apt-get -y update',
-  path    => '/usr/bin/env',
+  command  => 'apt-get -y update',
+  provider => 'shell',
 }
 exec {'Install Nginx':
-  command => 'apt-get -y install nginx',
-  path    => '/usr/bin/env',
+  command  => 'apt-get -y install nginx',
+  provider => 'shell',
 }
 exec {'Create test directory':
-  command => 'mkdir -p /data/web_static/releases/test/',
-  path    => '/usr/bin/env',
+  command  => 'mkdir -p /data/web_static/releases/test/',
+  provider => 'shell',
 }
 exec {'Create shared directory':
-  command => 'mkdir -p /data/web_static/shared/',
-  path    => '/usr/bin/env',
+  command  => 'mkdir -p /data/web_static/shared/',
+  provider => 'shell',
 }
 exec {'create index.html':
-  command => 'echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html',
-  path    => '/usr/bin/env',
+  command  => 'echo "Holberton School" > /data/web_static/releases/test/index.html',
+  provider => 'shell',
 }
 exec {'changed ownership':
-  command => 'chown -R ubuntu:ubuntu /data',
-  path    => '/usr/bin/env',
+  command  => 'chown -R ubuntu:ubuntu /data',
+  provider => 'shell',
 }
 exec {'create symbolic link':
-  command => 'ln -sf /data/web_static/releases/test /data/web_static/current',
-  path    => '/usr/bin/env',
+  command  => 'ln -sf /data/web_static/releases/test /data/web_static/current',
+  provider => 'shell',
 }
 exec {'change default':
-  command => 'sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
-  path    => '/usr/bin/env',
+  command  => 'sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+  provider => 'shell',
 }
 exec {'restart nginx sevice':
-  command => 'service nginx restart',
-  path    => '/usr/bin/env',
+  command  => 'service nginx restart',
+  provider => 'shell',
 }
