39f0689c173a47ad47158dd0e6f791a1e1cd9d98
diegojojoayandun <3922@holbertonschool.com>
2022-05-24 13:31:27 -0500

    fix output erros

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""Module using flask to get routes"""
+"""Task7 - states list"""
 
 from flask import Flask, render_template
 from models import *
 from models.state import State
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """GET information of route /states_list (Objects State)"""
+    """
+    route /states_list (Objects State)
+    """
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def storage_close(exception):
-    '''Close current session'''
+    '''
+    teardown close
+    '''
     storage.close()
 
 
